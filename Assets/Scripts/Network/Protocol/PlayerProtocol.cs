881d06001ff60e5c5d911f483da6acf60744cdd7
Horio Daichi <koppepan0114@gmail.com>
2015-11-18 21:32:45 +0900

    protocolをファイルわけ

---
+++ Assets/Scripts/Network/Protocol/PlayerProtocol.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Network
+{
+	public struct PlayerPosition
+	{
+		public float pos;
+		public PlayerPosition(float pos)
+		{
+			this.pos = pos;
+		}
+	}
+
+	public struct BulletFire
+	{
+		public float pos;
+		public BulletFire(float pos)
+		{
+			this.pos = pos;
+		}
+	}
+
+	public struct SyncHitPoint
+	{
+		public int hp;
+		public SyncHitPoint(int hp)
+		{
+			this.hp = hp;
+		}
+	}
+}
---
+++ Assets/Scripts/Network/Protocol/Protocol.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Network
+{
+	public enum ProtocolType
+	{
+		GotoMain,
+		GotoTitle,
+		TextOnly,
+		PlayerPosition,
+		BulletFire,
+		SyncHitPoint,
+		Dead,
+	};
+
+	interface IProtocol
+	{
+		void Execute();
+		void Pack(byte[] data);
+	}
+
+	public struct Msg
+	{
+		public ProtocolType type;
+		public byte[] data;
+
+		public Msg(ProtocolType type, byte[] data)
+		{
+			this.type = type;
+			this.data = data;
+		}
+		public Msg(ProtocolType type)
+		{
+			this.type = type;
+			data = null;
+		}
+	}
+
+	public struct TextMessage
+	{
+		public string text;
+		public TextMessage(string text)
+		{
+			this.text = text;
+		}
+	}
+}
